1f0fa5ea590ec44fb07fdcf35e178f6aee68e15d
Febri Rizki <febririzki95@gmail.com>
2018-07-29 16:06:55 +0700

    first commit

---
+++ helloworld.php
@@ -0,0 +1,21 @@
+<html>
+  <head>
+    <title>PHP runs on GAE</title>
+  </head>
+  <body>
+    <?php
+    echo "Tes, 1, 2, 3";
+    ?>
+    <h1>PHP is almost cool! &<div id="x1F50E">
+    </h1>
+    <!-- Opsional -->
+    <img>
+    src="https://memegenerator.net/img
+    /instances/67407853/not-sure-if-im
+    -bad-at-php-or-php-is-shit.jpg"
+    alt="I love PHP, but I am lying">
+    <!-- END:Opsional -->
+  </body>
+</html>
+
+  
